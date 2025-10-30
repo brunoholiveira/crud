a9da72fae6cad1ba6dd5dcb6942460ce454cdcbc
Matheus Gabriel da Silva <matheus.silva@crmbonus.com>
2025-10-30 16:04:03 -0300

    ADD UPDATE

--- controller/veiculo_controller.js
+++ controller/veiculo_controller.js
@@ -1,47 +1,72 @@
 import connect from "../config/connection.js";
 
-let veiculo = {}
-let con = await connect()
+let veiculo = {};
+let con = await connect();
 
 veiculo.create = async function (req, res) {
-    try {
-        let veiculo = req.body;
-        let sql = "INSERT INTO veiculo (placa_veiculo, modelo_veiculo, preco_veiculo) VALUES (?, ?, ?);";
-        let values = [veiculo.placa_veiculo, veiculo.modelo_veiculo, veiculo.preco_veiculo];
-        let result = await con.query(sql, values);
-        res.send({
-            status:"Insert efetuado com sucesso",
-            result:result
-        })
-    } catch (e) {
-        console.log("deu erro ao inserir", e)
-    }
-}
+  try {
+    let veiculo = req.body;
+    let sql =
+      "INSERT INTO veiculo (placa_veiculo, modelo_veiculo, preco_veiculo) VALUES (?, ?, ?);";
+    let values = [
+      veiculo.placa_veiculo,
+      veiculo.modelo_veiculo,
+      veiculo.preco_veiculo,
+    ];
+    let result = await con.query(sql, values);
+    res.send({
+      status: "Insert efetuado com sucesso",
+      result: result,
+    });
+  } catch (e) {
+    console.log("deu erro ao inserir", e);
+  }
+};
 
 veiculo.all = async function (req, res) {
-    try {
-        let sql = "SELECT * FROM veiculo;";
-        let result = await con.query(sql);
-        res.send(JSON.stringify({status:200, error:null, response: result[0]}));
-    } catch (e) {
-        console.log("deu erro ao consultar", e)
-    }
-}
+  try {
+    let sql = "SELECT * FROM veiculo;";
+    let result = await con.query(sql);
+    res.send(JSON.stringify({ status: 200, error: null, response: result[0] }));
+  } catch (e) {
+    console.log("deu erro ao consultar", e);
+  }
+};
 
 veiculo.delete = async function (req, res) {
-    try {
-        let veiculo = req.body;
-        let sql = "DELETE from veiculo where placa_veiculo = ?;";
-        let values = [veiculo.placa_veiculo];
-        let result = await con.query(sql, values);
-        res.send({
-            status:"Delete efetuado com sucesso",
-            result:result
-        })
-    } catch (e) {
-        console.log("deu erro ao deletar", e)
-    }
-}
-        
+  try {
+    let veiculo = req.body;
+    let sql = "DELETE from veiculo where placa_veiculo = ?;";
+    let values = [veiculo.placa_veiculo];
+    let result = await con.query(sql, values);
+    res.send({
+      status: "Delete efetuado com sucesso",
+      result: result,
+    });
+  } catch (e) {
+    console.log("deu erro ao deletar", e);
+  }
+};
+
+veiculo.update = async function (req, res) {
+  try {
+    let veiculo = req.body;
+    let sql =
+      "UPDATE veiculo SET placa_veiculo = ?, modelo_veiculo = ?, preco_veiculo = ? WHERE placa_veiculo = ?";
+    let values = [
+      veiculo.placa_veiculo,
+      veiculo.modelo_veiculo,
+      veiculo.preco_veiculo,
+      veiculo.placa_veiculo,
+    ];
+    let result = await con.query(sql, values);
+    res.send({
+      status: "UPDATE realizado com sucesso",
+      result: result,
+    });
+  } catch (e) {
+    console.log("Falha ao realizar o update");
+  }
+};
 
 export default veiculo;
