51df125d4cf4a3797f811a0d7e014ea03a844ad9
Matheus Gabriel da Silva <matheus.silva@crmbonus.com>
2025-10-30 14:27:31 -0300

    AJUSTE CHAMADAS

--- controller/veiculo_controller.js
+++ controller/veiculo_controller.js
@@ -18,13 +18,28 @@ veiculo.create = async function (req, res) {
     }
 }
 
-estudante.all = async function (req, res) {
+veiculo.all = async function (req, res) {
     try {
         let sql = "SELECT * FROM veiculo;";
         let result = await con.query(sql);
         res.send(JSON.stringify({status:200, error:null, response: result[0]}));
     } catch (e) {
         console.log("deu erro ao consultar", e)
+    }
+}
+
+veiculo.delete = async function (req, res) {
+    try {
+        let veiculo = req.body;
+        let sql = "DELETE from veiculo where placa_veiculo = ?;";
+        let values = [veiculo.placa_veiculo];
+        let result = await con.query(sql, values);
+        res.send({
+            status:"Delete efetuado com sucesso",
+            result:result
+        })
+    } catch (e) {
+        console.log("deu erro ao deletar", e)
     }
 }
         
